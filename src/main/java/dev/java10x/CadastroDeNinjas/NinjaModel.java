876b10a31ac1fb8d64b85dc78b7adfce8884a198
Mateus De La Fuente Cezar <mateusfuntecezar10@hotmail.com>
2025-11-06 13:46:01 -0300

    Cria uma entidade no banco de dados apartir do NinjaModel - Aula 03

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/NinjaModel.java
@@ -0,0 +1,70 @@
+package dev.java10x.CadastroDeNinjas;
+
+import jakarta.persistence.*;
+
+//Entity ele transforma uma classe em uma entidade do BD
+// JPA = Java Persistence API
+@Entity
+@Table(name = "tb_cadastro")
+public class NinjaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    Long id;
+    String nome ;
+    String cla ;
+    Character rank ;
+    String tecnicas;
+    int idade ;
+
+    public NinjaModel() {
+    }
+
+    public NinjaModel(String nome, String cla, Character rank, String tecnicas, int idade) {
+        this.nome = nome;
+        this.cla = cla;
+        this.rank = rank;
+        this.tecnicas = tecnicas;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCla() {
+        return cla;
+    }
+
+    public void setCla(String cla) {
+        this.cla = cla;
+    }
+
+    public Character getRank() {
+        return rank;
+    }
+
+    public void setRank(Character rank) {
+        this.rank = rank;
+    }
+
+    public String getTecnicas() {
+        return tecnicas;
+    }
+
+    public void setTecnicas(String tecnicas) {
+        this.tecnicas = tecnicas;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
