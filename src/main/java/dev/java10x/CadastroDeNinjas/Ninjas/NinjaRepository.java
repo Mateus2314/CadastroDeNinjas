3118b245d47775d0736dcac98ca00a118332bf65
Mateus De La Fuente Cezar <mateusfuntecezar10@hotmail.com>
2025-11-26 08:59:32 -0300

    chore: .gitignore adiciona as minhas credencias do db

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaRepository.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaRepository.java
@@ -1,11 +1,11 @@
 package dev.java10x.CadastroDeNinjas.Ninjas;
 
-import dev.java10x.CadastroDeNinjas.NinjaModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
 public interface NinjaRepository extends JpaRepository<NinjaModel, UUID> {
+
 
 
 
