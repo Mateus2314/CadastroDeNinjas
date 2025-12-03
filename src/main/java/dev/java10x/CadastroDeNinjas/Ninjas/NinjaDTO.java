6463cf758e22f0bb113590d0375a5845150fd902
Mateus De La Fuente Cezar <mateusfuntecezar10@hotmail.com>
2025-12-03 09:20:17 -0300

    Chore: Inclui a versao do DTO pela entidade e mapeia manualmente no NinjaMapper os caminhos entidade x dto.

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaDTO.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaDTO.java
@@ -1,4 +1,26 @@
 package dev.java10x.CadastroDeNinjas.Ninjas;
 
+import dev.java10x.CadastroDeNinjas.Missoes.MissoesModel;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class NinjaDTO {
+
+    private UUID id;
+    private String nome ;
+    private String cla ;
+    private Character rank ;
+    private String tecnicas;
+    private int idade ;
+    private String email;
+    private MissoesModel missoes ;
+    private String sobreNome ;
+
 }
--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaMapper.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaMapper.java
@@ -1,4 +1,42 @@
 package dev.java10x.CadastroDeNinjas.Ninjas;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class NinjaMapper {
+
+    public NinjaModel map(NinjaDTO ninjaDTO ){
+
+        NinjaModel ninjaModel = new NinjaModel();
+
+        ninjaModel.setId(ninjaDTO.getId());
+        ninjaModel.setNome(ninjaDTO.getNome());
+        ninjaModel.setCla(ninjaDTO.getCla());
+        ninjaModel.setRank(ninjaDTO.getRank());
+        ninjaModel.setTecnicas(ninjaDTO.getTecnicas());
+        ninjaModel.setIdade(ninjaDTO.getIdade());
+        ninjaModel.setEmail(ninjaDTO.getEmail());
+        ninjaModel.setMissoes(ninjaDTO.getMissoes());
+        ninjaModel.setSobreNome(ninjaDTO.getSobreNome());
+
+        return ninjaModel;
+    }
+
+    public NinjaDTO map(NinjaModel ninjaModel ){
+        NinjaDTO ninjaDTO = new NinjaDTO();
+
+        ninjaDTO.setId(ninjaModel.getId());
+        ninjaDTO.setNome(ninjaModel.getNome());
+        ninjaDTO.setCla(ninjaModel.getCla());
+        ninjaDTO.setRank(ninjaModel.getRank());
+        ninjaDTO.setTecnicas(ninjaModel.getTecnicas());
+        ninjaDTO.setIdade(ninjaModel.getIdade());
+        ninjaDTO.setEmail(ninjaModel.getEmail());
+        ninjaDTO.setMissoes(ninjaModel.getMissoes());
+        ninjaDTO.setSobreNome(ninjaModel.getSobreNome());
+
+        return ninjaDTO;
+
+    }
+
 }
